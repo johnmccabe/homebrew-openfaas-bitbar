71ae64376697b543067cbc09dfa8fff7581ace0b
John McCabe <john@johnmccabe.net>
2017-10-07 17:01:48 +0100

    Bump to version 0.0.4

--- Formula/openfaas-bitbar.rb
+++ Formula/openfaas-bitbar.rb
@@ -1,9 +1,9 @@
 class Openfaas-bitbar < Formula
     desc "Manage OpenFaaS from your OSX menubar"
     homepage "https://github.com/johnmccabe/openfaas-bitbar/"
-    url "https://github.com/johnmccabe/openfaas-bitbar/releases/download/0.0.3/openfaas-bitbar.tar.gz"
-    sha256 "62e1e3c3087efb6ab414d374134226c221252b8f538b331d171153461a534b4a"
-    version "0.0.3"
+    url "https://github.com/johnmccabe/openfaas-bitbar/releases/download/0.0.4/openfaas-bitbar.tar.gz"
+    sha256 "acc683a0efb7ebb149ead51e0fa0cf9845839385fff74b2ddc2149fda3b09f68"
+    version "0.0.4"
   
     bottle :unneeded
   
